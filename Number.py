9c9e087e0ffe16b719ee65d0b2946b25d0adaabe
abhisek-github <abhisek.mohanty11@gmail.com>
2019-10-02 22:05:45 +0530

    Abhi_local1

---
+++ Number.py
@@ -0,0 +1,13 @@
+#print(2**3)--print(0.2*3)--print(8//2)
+
+
+age='25'
+print("Happy"+" "+age+"th"+" "+"birthday")
+
+# or##
+
+Age1=25
+print("Happy"+" "+str(Age1)+"th"+" "+"Birthday Ashmin")
+
+print("Abhisek"+" "+str(4.0/2))
+
