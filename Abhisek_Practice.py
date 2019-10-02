3442f4fabf85cddfc39aa0e916c89f79a766e9d9
abhisek-github <abhisek.mohanty11@gmail.com>
2019-10-02 21:59:28 +0530

    Abhi_local

---
+++ Abhisek_Practice.py
@@ -0,0 +1,6 @@
+#print(10*'Abhisek')
+
+message=("Abhisek First Python Program")
+message1=("Hello Python World!!")
+print(message)
+print(message1)
---
+++ Chapter_List.py
@@ -0,0 +1,5 @@
+v_friends=['abc','dsf','fdsgf','gfgf']
+print(v_friends)
+print(v_friends[2])
+print(v_friends[0].title())
+print(v_friends[-1])
