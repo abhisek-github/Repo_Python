1e8c99783a19296238ee6068ba679f73f1a8d079
unknown <abhisek.mohanty11@gmail.com>
2019-10-03 16:16:03 +0530

    Abhi_Office_2

---
+++ Demo.py
@@ -0,0 +1,35 @@
+# variable_a="Abhisek"
+# #print(variable_b.title())
+# variable_b="abhisek mohanty"
+# print(variable_a.lower())
+# print(variable_b.title())
+# first_name="abhisek"
+# last_name="mohanty"
+# Full_Name=first_name+" "+last_name
+# #print(Full_Name)
+# #print(Full_Name.upper())
+# variable_3="How are you"
+#
+# print("Hello!"+" "+ Full_Name.title()+ "," + variable_3 + "?")
+# print("Hello!\n"+ Full_Name.title()+ "\n" + variable_3 + "?")
+#
+# print("\tFull_Name")
+
+print("Hello !\nAbhisek Mohanty\nHow r u ?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
